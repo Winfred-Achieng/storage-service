56831a965892e2fefcd4778b166ec951b47d2c38
winfred <achiengwinfred20@gmail.com>
2024-03-22 10:43:28 +0300

    initial commit

---
+++ src/main/java/com/winfred/multipartfile/entity/ImageData.java
@@ -0,0 +1,28 @@
+package com.winfred.multipartfile.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Image")
+public class ImageData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    @Lob
+    @Column(name = "imageData",length = 1000)
+    private byte [] imageData;
+}
